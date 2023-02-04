391f921489a963bd92df607a041121e708252f57
chucky23 <chucky23mc@gmail.com>
2023-02-04 22:44:40 +0300

    Install Obsidian

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Pizzdata',
-  tagline: 'Вкусно и просто о данных',
+  tagline: 'Вкусно о данных. Без снобизма и мата.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
