2af4495c486b07a22b065f06d5167f4bb28b834a
chucky23 <chucky23mc@gmail.com>
2023-02-05 15:45:31 +0300

    Add GTM

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,18 +42,21 @@ const config = {
           // Please change this to your repo.
           routeBasePath: '/',
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        googleTagManager: {
+          containerId: 'GTM-PQXP3JH',
         },
       }),
     ],
